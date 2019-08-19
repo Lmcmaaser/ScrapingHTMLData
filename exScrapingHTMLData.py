60c4f4980b173a22c0fc190e6226c779ff54cde2
Lmcmaaser <52054766+Lmcmaaser@users.noreply.github.com>
2019-08-19 13:36:11 -0700

    Update exScrapingHTMLData.py

--- exScrapingHTMLData.py
+++ exScrapingHTMLData.py
@@ -3,19 +3,19 @@ import urllib.request, urllib.parse, urllib.error
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl
-import re
 
-# Ignore SSL certificate errors
+
+#ignore SSL certificate errors
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter - ')
-html = urllib.request.urlopen(url, context=ctx).read() #can treat webpage like a file
-soup = BeautifulSoup(html, 'html.parser') #returns the object "soup"
+url = input('Enter - ') #enter webpage
+html = urllib.request.urlopen(url, context=ctx).read() #treast webpage like a file
+soup = BeautifulSoup(html, 'html.parser')
 
 numb = []
-tags = soup('span')  # Retrieve all of the span tags
+tags = soup('span')  #retrieves desired tags
 for tag in tags:
     numb.append(int(tag.string))
 print(sum(numb))
